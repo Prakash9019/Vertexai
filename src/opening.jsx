9c917451f5f347491d23d2537832175ba8346b73
Pentakotacharan <charanpentakota2006@gmail.com>
2025-01-09 05:59:30 +0530

    add

--- src/opening.jsx
+++ src/opening.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button } from "./components/Button";
+import { Button } from "./Components2/button";
 import { useNavigate } from 'react-router-dom';
 import logo from "./assets/logo.png"
 export function LandingHero() {
